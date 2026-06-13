6fda8c597218e3f902a24fd371fde8591ade491f
halokaryamedia-source <halokaryamedia@gmail.com>
2026-06-14 03:16:16 +0700

    b

--- EngineData/LauncherApp/session_reporting.py
+++
@@ -1,7 +0,0 @@
-"""Deprecated Python launcher module.
-
-TranslateIT active launcher has moved to EngineData/LauncherApp/RustApp.
-This file is intentionally inactive and must not be used as runtime entrypoint.
-"""
-
-raise RuntimeError("TranslateIT session reporting moved to RustApp. Use EngineData/LauncherApp/RustApp.")
