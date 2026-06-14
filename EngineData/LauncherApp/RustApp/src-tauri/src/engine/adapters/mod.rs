a30d8828ab9580bb36d542e0e793745981c07690
halokaryamedia-source <halokaryamedia@gmail.com>
2026-06-14 15:16:11 +0700

    Register realtime handoff adapter

--- EngineData/LauncherApp/RustApp/src-tauri/src/engine/adapters/mod.rs
+++ EngineData/LauncherApp/RustApp/src-tauri/src/engine/adapters/mod.rs
@@ -14,6 +14,7 @@ pub mod orchestration_logic;
 pub mod output_dry_run;
 pub mod pipeline_logic;
 pub mod playback_logic;
+pub mod realtime_handoff_logic;
 pub mod segment_flow_logic;
 pub mod session_logic;
 pub mod stream_ownership_logic;
