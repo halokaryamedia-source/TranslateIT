123c39a6c9c557a8a6ebd015e05633b13223cccf
halokaryamedia-source <halokaryamedia@gmail.com>
2026-06-15 23:56:12 +0700

    Import settings layout guard stylesheet

--- EngineData/LauncherApp/RustApp/src/main.ts
+++ EngineData/LauncherApp/RustApp/src/main.ts
@@ -1,7 +1,6 @@
 import "./styles.css";
+import "./settingsLayout.css";
 import { LauncherController } from "./app/launcher/launcherController";
 
 const app = document.querySelector<HTMLDivElement>("#app");
-if (!app) throw new Error("TranslateIT app root was not found.");
-
-new LauncherController(app).start();
+if (app) new LauncherController(app).start();
