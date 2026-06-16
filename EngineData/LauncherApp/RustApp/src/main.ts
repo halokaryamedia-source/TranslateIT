f2946a20979158cbfd06f6fd26e777aeb733f9ad
halokaryamedia-source <halokaryamedia@gmail.com>
2026-06-16 22:22:19 +0700

    ui: rollback composer control override import

--- EngineData/LauncherApp/RustApp/src/main.ts
+++ EngineData/LauncherApp/RustApp/src/main.ts
@@ -4,7 +4,6 @@ import "./launcherGuard.css";
 import "./professionalUi.css";
 import "./referenceLayout.css";
 import "./approvedReferenceLock.css";
-import "./composerControlFix.css";
 import { LauncherController } from "./app/launcher/launcherController";
 import { bindAttachmentLimitWatcher } from "./app/launcher/attachmentLimitWatcher";
 import { bindAudioPipelineResultWatcher as bindResultWatcher } from "./app/launcher/audioPipelineResultWatcher";
