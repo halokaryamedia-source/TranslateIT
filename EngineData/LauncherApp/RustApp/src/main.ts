dc8f3ad4e1b1f050bc7bb574b5dbb5c96fa373e3
halokaryamedia-source <halokaryamedia@gmail.com>
2026-06-16 23:48:19 +0700

    ui: import modular developer settings layout

--- EngineData/LauncherApp/RustApp/src/main.ts
+++ EngineData/LauncherApp/RustApp/src/main.ts
@@ -5,6 +5,7 @@ import "./referenceLayout.css";
 import "./mainPageLayout.css";
 import "./audioSettingsLayout.css";
 import "./translateSettingsLayout.css";
+import "./developerSettingsLayout.css";
 import { LauncherController } from "./app/launcher/launcherController";
 import { bindAttachmentLimitWatcher } from "./app/launcher/attachmentLimitWatcher";
 import { bindAudioPipelineResultWatcher as bindResultWatcher } from "./app/launcher/audioPipelineResultWatcher";
