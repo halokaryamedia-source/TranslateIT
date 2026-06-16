a6fe74c927c40af355bba3ac1fdc79eab7a929eb
halokaryamedia-source <halokaryamedia@gmail.com>
2026-06-16 23:41:13 +0700

    ui: import modular audio settings layout

--- EngineData/LauncherApp/RustApp/src/main.ts
+++ EngineData/LauncherApp/RustApp/src/main.ts
@@ -3,6 +3,7 @@ import "./launcherGuard.css";
 import "./professionalUi.css";
 import "./referenceLayout.css";
 import "./mainPageLayout.css";
+import "./audioSettingsLayout.css";
 import { LauncherController } from "./app/launcher/launcherController";
 import { bindAttachmentLimitWatcher } from "./app/launcher/attachmentLimitWatcher";
 import { bindAudioPipelineResultWatcher as bindResultWatcher } from "./app/launcher/audioPipelineResultWatcher";
