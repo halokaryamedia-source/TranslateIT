9b6fc51c3dbee602f117fff020c9cf65f5b69b6e
halokaryamedia-source <halokaryamedia@gmail.com>
2026-06-17 00:14:12 +0700

    ui: import modular general settings layout

--- EngineData/LauncherApp/RustApp/src/main.ts
+++ EngineData/LauncherApp/RustApp/src/main.ts
@@ -3,6 +3,7 @@ import "./launcherGuard.css";
 import "./professionalUi.css";
 import "./referenceLayout.css";
 import "./mainPageLayout.css";
+import "./generalSettingsLayout.css";
 import "./audioSettingsLayout.css";
 import "./translateSettingsLayout.css";
 import "./developerSettingsLayout.css";
