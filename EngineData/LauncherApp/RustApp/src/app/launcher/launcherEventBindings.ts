58303bcc2d36642aa7c09da127b91944ef1a43e2
halokaryamedia-source <halokaryamedia@gmail.com>
2026-06-18 16:46:25 +0700

    refactor: make launcher event bindings disposable

--- EngineData/LauncherApp/RustApp/src/app/launcher/launcherEventBindings.ts
+++ EngineData/LauncherApp/RustApp/src/app/launcher/launcherEventBindings.ts
@@ -22,34 +22,39 @@ export type LauncherEventHandlers = {
   applyRuntimeSettings: (settings: RuntimeSettings) => void;
 };
 
-export function bindLauncherEvents(ui: UiRefs, handlers: LauncherEventHandlers): void {
-  ui.settingsButton.addEventListener("click", handlers.showSettings);
-  ui.backHomeButton.addEventListener("click", handlers.showHome);
-  ui.microphoneButton.addEventListener("click", () => void handlers.startOrStopRecording());
-  ui.quickMicButton.addEventListener("click", () => void handlers.startOrStopRecording());
-  ui.recordStatusButton.addEventListener("click", () => void handlers.startOrStopRecording());
-  ui.sendButton.addEventListener("click", () => void handlers.submitText());
-  ui.messageInput.addEventListener("input", handlers.resizeMessageInput);
+export function bindLauncherEvents(ui: UiRefs, handlers: LauncherEventHandlers): () => void {
+  const controller = new AbortController();
+  const options = { signal: controller.signal };
+
+  ui.settingsButton.addEventListener("click", handlers.showSettings, options);
+  ui.backHomeButton.addEventListener("click", handlers.showHome, options);
+  ui.microphoneButton.addEventListener("click", () => void handlers.startOrStopRecording(), options);
+  ui.quickMicButton.addEventListener("click", () => void handlers.startOrStopRecording(), options);
+  ui.recordStatusButton.addEventListener("click", () => void handlers.startOrStopRecording(), options);
+  ui.sendButton.addEventListener("click", () => void handlers.submitText(), options);
+  ui.messageInput.addEventListener("input", handlers.resizeMessageInput, options);
   ui.messageInput.addEventListener("keydown", (event) => {
     if (event.key === "Enter" && !event.shiftKey) {
       event.preventDefault();
       void handlers.submitText();
     }
-  });
-  ui.newChatButton.addEventListener("click", () => void handlers.createNewChat());
-  ui.composerPlusButton.addEventListener("click", handlers.openAttachmentInput);
-  ui.attachmentInput.addEventListener("change", () => void handlers.ingestAttachmentFiles());
+  }, options);
+  ui.newChatButton.addEventListener("click", () => void handlers.createNewChat(), options);
+  ui.composerPlusButton.addEventListener("click", handlers.openAttachmentInput, options);
+  ui.attachmentInput.addEventListener("change", () => void handlers.ingestAttachmentFiles(), options);
   handlers.bindAttachmentDropZone();
-  ui.recentChatButton.addEventListener("click", () => void handlers.showRecentChat());
-  ui.unsavedChatButton.addEventListener("click", () => void handlers.showUnsavedChat());
-  ui.savedChatButton.addEventListener("click", () => void handlers.showSavedChat());
-  ui.localDataButton.addEventListener("click", () => void handlers.showLocalData());
-  ui.micOptionsButton.addEventListener("click", handlers.openAudioSettings);
-  ui.voiceOutputButton.addEventListener("click", handlers.toggleVoiceOutput);
-  ui.voiceOptionsButton.addEventListener("click", handlers.openAudioSettings);
-  ui.settingsNavItems.forEach((button) => button.addEventListener("click", () => handlers.renderSettingsTab(button.dataset.settingsTab as SettingsTab)));
+  ui.recentChatButton.addEventListener("click", () => void handlers.showRecentChat(), options);
+  ui.unsavedChatButton.addEventListener("click", () => void handlers.showUnsavedChat(), options);
+  ui.savedChatButton.addEventListener("click", () => void handlers.showSavedChat(), options);
+  ui.localDataButton.addEventListener("click", () => void handlers.showLocalData(), options);
+  ui.micOptionsButton.addEventListener("click", handlers.openAudioSettings, options);
+  ui.voiceOutputButton.addEventListener("click", handlers.toggleVoiceOutput, options);
+  ui.voiceOptionsButton.addEventListener("click", handlers.openAudioSettings, options);
+  ui.settingsNavItems.forEach((button) => button.addEventListener("click", () => handlers.renderSettingsTab(button.dataset.settingsTab as SettingsTab), options));
   window.addEventListener(RUNTIME_SETTINGS_SAVED_EVENT, (event) => {
     const settings = (event as CustomEvent<RuntimeSettings>).detail;
     if (settings) handlers.applyRuntimeSettings(settings);
-  });
+  }, options);
+
+  return () => controller.abort();
 }
