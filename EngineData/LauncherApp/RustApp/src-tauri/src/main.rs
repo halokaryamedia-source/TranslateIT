4c1676ef349363caa3c350f2bd5c09d8066796a5
halokaryamedia-source <halokaryamedia@gmail.com>
2026-06-18 11:38:17 +0700

    Register Audio Studio take listing command

--- EngineData/LauncherApp/RustApp/src-tauri/src/main.rs
+++ EngineData/LauncherApp/RustApp/src-tauri/src/main.rs
@@ -5,6 +5,7 @@ use commands::audio::{get_input_status, list_audio_devices};
 use commands::audio_studio::{
     audio_studio_export_project_metadata,
     audio_studio_import_take,
+    audio_studio_list_takes,
     audio_studio_stage_guided_take,
     audio_studio_update_take_state,
 };
@@ -35,6 +36,7 @@ fn main() {
         audio_studio_import_take,
         audio_studio_stage_guided_take,
         audio_studio_update_take_state,
+        audio_studio_list_takes,
         audio_studio_export_project_metadata,
     ]);
 
