7bd45c9833af374eb6b4df09c237b439760038d5
halokaryamedia-source <halokaryamedia@gmail.com>
2026-08-10 02:56:07 +0700

    Retire manual translation engine modules

--- EngineData/Frontend/RustApp/src-tauri/src/engine/mod.rs
+++ EngineData/Frontend/RustApp/src-tauri/src/engine/mod.rs
@@ -11,8 +11,6 @@ pub mod hardware;
 pub mod history_store;
 pub mod inference;
 pub mod logging;
-pub mod manual_translation;
-pub mod manual_translation_accelerated;
 pub mod models;
 pub mod native_execution;
 pub mod native_runners;
@@ -31,6 +29,5 @@ pub mod transcript;
 pub mod transcript_session;
 
 pub use capture_lifecycle::{start_capture, stop_capture};
-pub use manual_translation_accelerated::translate_text;
 pub use runtime_settings::{load_settings, save_default_settings};
 pub use status_runtime::{current_status, live_capture_runtime_status, runtime_diagnostics};
