ebeffbe39b2fe5a3158af36e68256fb0fda84ebb
halokaryamedia-source <halokaryamedia@gmail.com>
2026-08-10 00:31:04 +0700

    Gate first launch through focused setup

--- EngineData/Frontend/RustApp/src/main.ts
+++ EngineData/Frontend/RustApp/src/main.ts
@@ -4,10 +4,12 @@ import "./professionalUi.css";
 import "./referenceLayout.css";
 import "./mainPageLayout.css";
 import "./historyLayout.css";
+import "./firstSetupLayout.css";
 import "./audioSettingsLayout.css";
 import "./translateSettingsLayout.css";
 import "./developerSettingsLayout.css";
 import { SimpleLauncherController } from "./app/simple-launcher/SimpleLauncherController";
+import { startDesktopWithFirstSetup } from "./app/first-setup/FirstSetupBootstrap";
 import { restoreNativeWindow } from "./app/active-launcher/windowRescue";
 import { installStartupDiagnostics, startupTrace } from "./app/active-launcher/startupDiagnostics";
 
@@ -24,4 +26,4 @@ window.setTimeout(() => {
 const app = document.querySelector<HTMLDivElement>("#app");
 if (!app) throw Error("TranslateIT app root was not found.");
 
-new SimpleLauncherController(app).start();
+void startDesktopWithFirstSetup(app, () => new SimpleLauncherController(app).start());
