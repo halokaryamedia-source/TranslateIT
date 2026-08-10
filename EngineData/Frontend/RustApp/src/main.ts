68b225e550d85f0d76f90f4d6c18280865de7af5
halokaryamedia-source <halokaryamedia@gmail.com>
2026-08-10 16:45:48 +0700

    Wire Meeting live activity presentation

--- EngineData/Frontend/RustApp/src/main.ts
+++ EngineData/Frontend/RustApp/src/main.ts
@@ -3,12 +3,14 @@ import "./launcherGuard.css";
 import "./professionalUi.css";
 import "./referenceLayout.css";
 import "./mainPageLayout.css";
+import "./meetingLiveActivity.css";
 import "./historyLayout.css";
 import "./firstSetupLayout.css";
 import "./audioSettingsLayout.css";
 import "./translateSettingsLayout.css";
 import "./developerSettingsLayout.css";
 import { SimpleLauncherController } from "./app/simple-launcher/SimpleLauncherController";
+import { startMeetingLiveActivityPresentation } from "./app/simple-launcher/MeetingLiveActivityPresentation";
 import { startDesktopWithFirstSetup } from "./app/first-setup/FirstSetupBootstrap";
 import { restoreNativeWindow } from "./app/active-launcher/windowRescue";
 import { installStartupDiagnostics, startupTrace } from "./app/active-launcher/startupDiagnostics";
@@ -26,4 +28,8 @@ window.setTimeout(() => {
 const app = document.querySelector<HTMLDivElement>("#app");
 if (!app) throw Error("TranslateIT app root was not found.");
 
-void startDesktopWithFirstSetup(app, () => new SimpleLauncherController(app).start());
+void startDesktopWithFirstSetup(app, () => {
+  const controller = new SimpleLauncherController(app);
+  controller.start();
+  startMeetingLiveActivityPresentation();
+});
